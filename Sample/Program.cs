e7e14fada996bd11fa932f61bce35e3757fb1172
eyupaopao <chen76543210@qq.com>
2024-05-13 16:59:23 +0800

    todo 播放iwaveprovider

--- Sample/Program.cs
+++ Sample/Program.cs
@@ -6,13 +6,4 @@ using NAudio.Wave;
 using NAudio.Wave.SampleProviders;
 
 var audioReader = new Mp3Reader("D:\\User\\Music\\F.I.R\\飞儿乐团 - 我们的爱.mp3");
-var buffer = new byte[1024 * 1024];
-Wave32To16Stream
-while (true)
-{
 
-    var len = audioReader.Read(buffer);
-    Console.WriteLine(len);
-    if (len == 0)
-        break;
-}
