2ac0b6c8deb0efa1632bd52664a93b308d9caa6d
Color_yr <402067010@qq.com>
2025-09-17 11:02:29 +0800

    Update MediaConverter.cs

--- Eyu.Audio/Utils/MediaConverter.cs
+++ Eyu.Audio/Utils/MediaConverter.cs
@@ -3,38 +3,50 @@ using NAudio.MediaFoundation;
 using NAudio.Wave;
 using NLayer;
 using System.IO;
+using System.Threading;
+using System.Threading.Tasks;
 
-namespace Eyu.Audio.Utils
+namespace Eyu.Audio.Utils;
+
+public static class MediaConverter
 {
-    public static class MediaConverter
+    public static bool MediaToMp3(WaveStream reader, string outPutFilePath)
     {
-
-
-        public static bool MediaToMp3(WaveStream reader, string outPutFilePath)
+        try
         {
-            try
-            {
-                MediaFoundationApi.Startup();
-                MediaFoundationEncoder.EncodeToMp3(reader, outPutFilePath);
-                MediaFoundationApi.Shutdown();
-                return true;
-            }
-            catch { return false; }
-
+            MediaFoundationApi.Startup();
+            MediaFoundationEncoder.EncodeToMp3(reader, outPutFilePath);
+            MediaFoundationApi.Shutdown();
+            return true;
         }
-        public static bool ConvertWavToMp3(WaveStream reader, string mp3FilePath)
+        catch { return false; }
+
+    }
+    public static bool ConvertWavToMp3(WaveStream reader, string mp3FilePath)
+    {
+        try
         {
+            using var writer = new LameMP3FileWriter(mp3FilePath, reader.WaveFormat, LAMEPreset.ABR_320);
+            reader.CopyTo(writer);
+            return true;
+        }
+        catch 
+        {
+            return false; 
+        }
+    }
 
-            try
-            {
-                using (var writer = new LameMP3FileWriter(mp3FilePath, reader.WaveFormat, LAMEPreset.ABR_128))
-                {
-                    reader.CopyTo(writer);
-                    return true;
-                }
-
-            }
-            catch { return false; }
+    public static async Task<bool> ConvertWavToMp3Async(WaveStream reader, string path, CancellationToken token)
+    {
+        try
+        {
+            using var writer = new LameMP3FileWriter(path, reader.WaveFormat, LAMEPreset.ABR_320);
+            await reader.CopyToAsync(writer, token);
+            return true;
+        }
+        catch
+        {
+            return false;
         }
     }
 }
